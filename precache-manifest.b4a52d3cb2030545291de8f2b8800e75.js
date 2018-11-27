7c13822438e3c050fb26fa9858f490b86e86a34b
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-27 14:53:08 +0100

    Updates

---
+++ precache-manifest.b4a52d3cb2030545291de8f2b8800e75.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2714f520c203183b683",
+    "url": "/KrystianKaminski/todo-app/static/js/runtime~main.f2714f52.js"
+  },
+  {
+    "revision": "ddab35852f739a31e2a0",
+    "url": "/KrystianKaminski/todo-app/static/js/main.ddab3585.chunk.js"
+  },
+  {
+    "revision": "6e606b0176b68e213992",
+    "url": "/KrystianKaminski/todo-app/static/js/1.6e606b01.chunk.js"
+  },
+  {
+    "revision": "29c20aade708b3e1629c923a53c57bbe",
+    "url": "/KrystianKaminski/todo-app/index.html"
+  }
+];
